f4884fad192cbe5dc5a040cc9f11806aafb69147
Chetan Bhadane <69416667+chetandsbhadane@users.noreply.github.com>
2024-05-30 10:57:22 +0530

    Add files via upload

---
+++ DockerAssign1/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("D2 developer");
+		System.out.println("Hello World");
+		System.out.println("By D1 developer");
+		System.out.println("This is from CDAC ACTS");
+		System.out.println("We are in DAC");
+	}
+}
